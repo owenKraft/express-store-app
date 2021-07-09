1249669da4de2eac747fd7f81e9b51c39f2be9f7
Owen Kraft <owenkraft7@gmail.com>
2021-07-08 20:41:25 -0600

    attempt content-security-policy fix by changing helmet config

--- app.js
+++ app.js
@@ -27,7 +27,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(compression());
-app.use(helmet());
+app.use(helmet({
+  contentSecurityPolicy: false,
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
